dcd0f9f2272a35955d4216447a4529718189674d
Brigitta Sipőcz <bsipocz@gmail.com>
2021-11-03 16:27:06 -0700

    We may want to use custom repo names

--- migrate_repo.sh
+++ migrate_repo.sh
@@ -1,13 +1,18 @@
+# The script requires the GitHub command line client to be installed, see
+# https://github.com/cli/cli
+
 if [[ -z $EXTENSION ]]; then
     EXTENSION=.git
 fi
 
-repo_name=$(basename $(pwd) ${EXTENSION})
+if [[ -z $REPO_NAME ]]; then
+    REPO_NAME=$(basename $(pwd) ${EXTENSION})
+fi
 
 if [[ -z $ORG_NAME ]]; then
-    remote_repo=${repo_name}
+    remote_repo=${REPO_NAME}
 else
-    remote_repo=${ORG_NAME}/${repo_name}
+    remote_repo=${ORG_NAME}/${REPO_NAME}
 fi
 
 gh repo create --private --confirm ${remote_repo}
